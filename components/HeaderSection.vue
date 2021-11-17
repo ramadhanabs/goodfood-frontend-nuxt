0441de046778394cb31a632b79efad619d100c06
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2021-11-17 10:01:53 +0700

    Add Header

---
+++ components/HeaderSection.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="bg-cover bg-opacity-50 py-5" style="background-image: url('/_nuxt/assets/image/bg-pattern-transparent.png');">
+  <div class="font-rubik mt-24 md:mt-16 px-5 md:px-20 md:grid md:grid-cols-2">
+    <div class="grid grid-cols-1 md:place-content-center">
+      <h1 class="text-4xl 2xl:text-6xl font-semibold text-green-primary leading-snug mt-10 mb-5">Good Food Us <br/> Good Mood</h1>
+      <img src="../assets/image/header-image-mobile.png" alt="header-mobile" class="md:hidden">
+      <div class="md:pr-20">
+        <p class="text-base text-gray-light font-light">I would think that conserving our natural resources should be a conservative position: Not to waste food, and not to throw away a lot of the food that we buy.</p>
+      </div>
+      <div class="flex my-5 gap-3">
+        <button class="bg-green-primary text-white text-sm rounded-full py-2 px-5 hover:bg-green-hover transition">Daftar Sekarang</button>
+        <button class="bg-gray-200 text-gray-700 text-sm rounded-full py-2 px-5 hover:bg-gray-300 transition">About Us</button>
+      </div>
+    </div>
+    <div class="hidden md:block">
+      <img src="../assets/image/header-image-desktop.png" alt="header-desktop">
+    </div>
+  </div>
+</div>
+</template>
