46d77d92a2d2fa2d7c08212a4b4b986815c33785
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2021-11-17 10:02:00 +0700

    Add Bottom Navbar

---
+++ components/BottomNavigationBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="grid grid-cols-3 border-t border-gray-100 w-100 px-5 py-5 font-rubik text-xs font-normal text-gray-500 text-center md:hidden">
+    <div>
+      <div class="mb-2">
+        <font-awesome-icon :icon="['fas', 'bars']" class="text-2xl mx-6"  />
+      </div>
+      <p>Home</p>
+    </div>
+    <div>
+      <div class="mb-2">
+        <font-awesome-icon :icon="['fas', 'bars']" class="text-2xl mx-6"  />
+      </div>
+      <p>Promotions</p>
+    </div>
+    <div>
+      <div class="mb-2">
+        <font-awesome-icon :icon="['fas', 'bars']" class="text-2xl text-center"  />
+      </div>
+      <p>Other</p>
+    </div>
+  </div>
+</template>
