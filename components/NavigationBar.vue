5f2fd7f16bdb6c6275bac4d4245efd99dcb7cbd4
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2021-11-17 10:02:09 +0700

    Add Navigation Bar

---
+++ components/NavigationBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="border-b bg-white border-gray-100 w-full px-5 md:px-20 py-5 font-rubik fixed top-0 z-10">
+    <div class="grid grid-cols-2 md:grid-cols-3">
+      <div class="flex content-center">
+        <div class="mt-1">
+          <img src="../assets/image/logo-elemes.png" alt="logo-elemes" class="w-28">
+        </div>
+      </div>
+      <div class="hidden md:block mt-1">
+        <div class="flex flex-row gap-x-8 content-center">
+          <div>
+            <a href="#" class="text-sm text-gray-600 hover:text-gray-700 transition">Home</a>
+          </div>
+          <div>
+            <a href="#" class="text-sm text-gray-600 hover:text-gray-700 transition">About</a>
+          </div>
+          <div>
+            <a href="#" class="text-sm text-gray-600 hover:text-gray-700 transition">Promotions</a>
+          </div>
+          <div>
+            <a href="#" class="text-sm text-gray-600 hover:text-gray-700 transition">Blogs</a>
+          </div>
+          <div>
+            <a href="#" class="text-sm text-gray-600 hover:text-gray-700 transition">Contact Us</a>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-end">
+        <a href="#" class="text-gray-600 text-sm rounded-full py-2 px-5 hover:text-green-hover transition hidden lg:block">Masuk</a>
+        <button class="bg-green-primary text-white text-sm rounded-full py-2 px-5 hover:bg-green-hover transition">Daftar Sekarang</button>
+      </div>
+    </div>
+  </div>
+</template>
