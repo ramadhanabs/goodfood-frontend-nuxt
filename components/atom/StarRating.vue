ec404fee870370f2277ef2c9a593f81b008ab90a
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2021-11-17 10:02:21 +0700

    Add Star Rating Component

---
+++ components/atom/StarRating.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mt-5 gap-0">
+    <font-awesome-icon :icon="['fas', 'star']" v-for="star, index in maxStar" :key="index" :class="[index < rating ? active : inactive]" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    rating: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      check: true,
+      active: 'text-yellow-300',
+      inactive: 'text-gray-300',
+      maxStar: 5,
+    };
+  },
+  computed: {
+    remainingStars() {
+      return this.maxStar - this.rating;
+    },
+  },
+};
+</script>
