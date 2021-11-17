986581d4df320fde43c713dcd50718500e43b63d
ramadhanabs <45602484+ramadhanabs@users.noreply.github.com>
2021-11-17 10:00:50 +0700

    Index Page

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <NavigationBar />
+  <HeaderSection />
+  <carousel-section />
+  <trending-section />
+  <BottomNavigationBar />
+  <FooterSection />
+</div>
+</template>
+
+<script>
+import CarouselSection from '../components/carousel/CarouselSection.vue';
+import TrendingSection from '../components/gallery/TrendingSection.vue';
+
+export default {
+  components: { TrendingSection, CarouselSection },
+};
+</script>
